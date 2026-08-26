79003258b1c5d1b4b4a29b4bc5dd1b457b529f5c
bernardo-rafa <rafa02bernardo@gmail.com>
2026-08-25 21:59:09 -0300

    corrigindo erros no app.tsx e Splash screen.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -4,8 +4,8 @@ export default function Page() {
   return (
     <View style={styles.container}>
       <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
+        <Text style={styles.title}>Fiel Torcida Fernandopolis</Text>
+        <Text style={styles.subtitle}> Faça Parte deste bando de loucos</Text>
       </View>
     </View>
   );
